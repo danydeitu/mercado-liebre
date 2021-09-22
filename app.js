53493df1a2520478f9280eb61f4a98ea632e4920
danydeitu <danydeitu18@gmail.com>
2021-09-22 10:42:28 -0300

    subo posicionamiento avanzado flebox

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(express.static(publicFolderPath));
 app.get('/',(req,res)=>
     res.sendFile(path.resolve(__dirname, './views/home.html')));
 
-  /*  app.get('/',Ofertas,(req,res)=>
-    res.sendFile(path.join(__dirname,'views','Ofertas.html')));
+   app.get('/register',(req,res)=>
+    res.sendFile(path.join(__dirname,'views','./register.html')));
 
-    app.get('/',TiendasOficiales,(req,res)=>
-    res.sendFile(path.join(__dirname,'views','Tiendas Oficiales.html')));   
-
+    app.get('/login',(req,res)=>
+    res.sendFile(path.join(__dirname,'views','login.html')));   
+/*
 app.get('/',Ayuda,(req,res)=>
     res.sendFile(path.join(__dirname,'views','Ayuda.html')));*/
 
