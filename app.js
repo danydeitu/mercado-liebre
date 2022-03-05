40d630d30cef2da56155ae2cbc1221ef9d7c2e65
Daniel <danydeitu18@gmail.com>
2022-03-05 16:05:18 -0300

    saco php

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-const port=80
+const port=80;
 const publicFolderPath=path.resolve(__dirname, './public');
 
 app.use(express.static(publicFolderPath));
