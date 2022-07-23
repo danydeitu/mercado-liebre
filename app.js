2fbb0a5c7351723db10286d2017fb512130041a1
Daniel <danydeitu18@gmail.com>
2022-07-23 13:46:44 -0300

    cambio variables de entorno

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const express=require('express');
+const express=require('express');r
 const app=express();
 const path=require('path');
-<<<<<<< HEAD
-const Port= process.env.Port || 3030
-=======
-const port=3000;
->>>>>>> bb5ef8bc409730d6a3be6446f13d1f92dce9e891
 const publicFolderPath=path.resolve(__dirname, './public');
-
-app.use(express.static(publicFolderPath));
-
 app.get('/',(req,res)=>
     res.sendFile(path.resolve(__dirname, './views/home.html')));
 
@@ -28,12 +20,4 @@ app.listen(process.env.PORT || 3000 , function() {
     res.sendFile(path.join(__dirname,'views','Ayuda.html')));*/
     
 
-<<<<<<< HEAD
-app.listen(port,()=>{
-    console.log(`servidor corriendo en el puerto`),Port});
-=======
-/*app.listen(port,()=>{
-    console.log(`servidor corriendo en http://localhost:${port}`)});*/
-
-   
->>>>>>> bb5ef8bc409730d6a3be6446f13d1f92dce9e891
+    console.log(`servidor corriendo en el puerto`,Port);
