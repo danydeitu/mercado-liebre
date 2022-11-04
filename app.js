0a6a181bcf142a32fe12fe1f983a9e86ef34fff7
Daniel <danydeitu18@gmail.com>
2022-11-04 15:29:12 -0300

    configuro para heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express=require('express');r
-const app=express();
-const path=require('path');
-const publicFolderPath=path.resolve(__dirname, './public');
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicFolderPath=path.resolve(__dirname,'./public');
 
+app.use(express.static('public'));
+app.set('puerto',process.env.PORT || 3000);
 app.get('/',(req,res)=>
-    res.sendFile(path.resolve(__dirname, './views/home.html')));
+    res.sendFile(path.resolve(__dirname,'./views/home.html')));
 
    app.get('/register',(req,res)=>
     res.sendFile(path.join(__dirname,'views','./register.html')));
@@ -18,12 +20,7 @@ app.listen(process.env.PORT || 3000 , function() {
 })
 
 
-    
-
-<<<<<<< HEAD
-/*app.listen(port,()=>{
+    /*
+app.listen(port,()=>{
     console.log(`servidor corriendo en http://localhost:${port}`)});*/
 
-=======
-    console.log(`servidor corriendo en el puerto`,Port);
->>>>>>> 2fbb0a5c7351723db10286d2017fb512130041a1
