1a646411ee092983708803513042603b8e7be81c
Daniel <danydeitu18@gmail.com>
2022-11-30 00:55:31 -0300

    modifico fuentes

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.get('/',(req,res)=>
     app.get('/login',(req,res)=>
     res.sendFile(path.join(__dirname,'views','login.html')));  
 
-    
+   
+
 app.listen(process.env.PORT || 3000 , function() {
     console.log(`Servidor corriendo en puerto 3000`);
 })
